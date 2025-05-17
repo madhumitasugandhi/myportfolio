58839847c2b890f0e1a54826b59cb278e6b984b3
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-17 23:22:23 +0530

    About.jsx 30% work completed.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+   <section id='about'
+   className='py-4 px[7vw] md:px-[7vw] lg:px-[20vx] font-sans mt-16 md:mt-24 lg:mt-32'
+    >
+      <div className='flex flex-col-reverse md:flex-row justify-between items-center'>
+    <div className='md:w-1/2 text-center md:text-left mt-8 md:mt-0'>
+    <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold text-white mb-2 leading-tight'>
+      Hello, I am 
+    </h1>
+    <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight'>
+      Madhumita Sugandhi
+    </h1>
+    </div>
+      </div>
+   </section>
   )
 }
 
