ed15702bb2b49607ee1ac8edf13f826bbec51e29
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-17 22:10:13 +0530

    Navbar work complete with responsiveness.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,21 +4,21 @@ import { FiMenu, FiX } from 'react-icons/fi';
 
 const Navbar = () => {
 
-  const [activeSection, setActiveSection]=useState("");
+  const [activeSection, setActiveSection] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
 
-  useEffect(()=>{
-    const handleScroll =()=>{
-      setIsScrolled(window.scrollY >50);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
     };
 
-    window.addEventListener("scroll",handleScroll);
-    return () => window.removeEventListener("scroll",handleScroll);
-  },[]);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-  const handleMenuItemClick=(SelectionId) =>{
+  const handleMenuItemClick = (SelectionId) => {
     setActiveSection(SelectionId);
     setIsOpen(false);
   }
@@ -32,12 +32,12 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7x] lg:px-[20vw] ${isScrolled ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shodow-md": "bg-transparent"
+    <nav className={`fixed top-10 w-full z-50 transition duration-300 px-[vw] md:px-[7x] lg:px-[20vw] ${isScrolled ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shodow-md" : "bg-transparent"
 
-    }`}>
-      <div className='text-white py-5 flex justify-between items-center'>
+      }`}>
+      <div className='text-white  py-5 flex justify-between items-center'>
 
-        <div className='text-lg font-semibold cursor-pointer '>
+        <div className='text-xl font-semibold cursor-pointer '>
           <span className='text-[#8245ec]'>&lt;</span>
           <span className='text-white'>Madhumita</span>
           <span className='text-[#8245ec]'>/</span>
@@ -48,7 +48,7 @@ const Navbar = () => {
 
         <ul className='hidden md:flex space-x-8 text-gray-300'>
           {menuItems.map((item) => (
-            <li key={item.id} className={`cursor-pointer hover:text-[#8245ec] ${ activeSection== item.id?"text-[#8245ec]": ""}`}>
+            <li key={item.id} className={`cursor-pointer hover:text-[#8245ec] ${activeSection == item.id ? "text-[#8245ec]" : ""}`}>
               <button onClick={() => handleMenuItemClick(item.id)}>
                 {item.label}
               </button>
@@ -57,44 +57,60 @@ const Navbar = () => {
         </ul>
         <div className='hidden md:flex space-x-4'>
           <a href="https://github.com/madhumitasugandhi"
-          target='_blank' 
-          rel='noopener noreferre'
-          className='text-gray-300 hover:text-[#8245ec]'
+            target='_blank'
+            rel='noopener noreferre'
+            className='text-gray-300 hover:text-[#8245ec]'
           >
-            <FaGithub size={24}/>
+            <FaGithub size={24} />
           </a>
           <a href="https://www.linkedin.com/in/madhumita-sugandhi-557aa22a9/"
-          target='_blank' 
-          rel='noopener noreferre'
-          className='text-gray-300 hover:text-[#8245ec]'
+            target='_blank'
+            rel='noopener noreferre'
+            className='text-gray-300 hover:text-[#8245ec]'
           >
-            <FaLinkedin size={24}/>
+            <FaLinkedin size={24} />
           </a>
         </div>
 
         <div className='md:hidden'>
           {
-            isOpen? (
+            isOpen ? (
               <FiX className="text-3xl text-[#8245ec] cursor-pointer"
-              onClick={()=> setIsOpen(false)}/>
-            ):(
+                onClick={() => setIsOpen(false)} />
+            ) : (
               <FiMenu className="text-3xl text-[#8245ec] cursor-pointer"
-              onClick={()=> setIsOpen(true)}/>
+                onClick={() => setIsOpen(true)} />
             )
 
           }
         </div>
       </div>
-      { isOpen && (
+      {isOpen && (
         <div className='absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg'>
           <ul className='flex flex-col items-center space-y-4 py-4 text-gray-300'>
-            {menuItems.map((item)=>(
-              <li key={item.id} className={`cursor-pointer hover:text-white ${activeSection === item.id ? "text-[#8245ec]": ""}`}>
-                <button onClick={()=> handleMenuItemClick(item.id)}>
+            {menuItems.map((item) => (
+              <li key={item.id} className={`cursor-pointer hover:text-white ${activeSection === item.id ? "text-[#8245ec]" : ""}`}>
+                <button onClick={() => handleMenuItemClick(item.id)}>
                   {item.label}
                 </button>
               </li>
             ))}
+            <div className='flex space-x-4'>
+              <a href="https://github.com/madhumitasugandhi"
+                target='_blank'
+                rel='noopener noreferre'
+                className='text-gray-300 hover:text-white'
+              >
+                <FaGithub size={24} />
+              </a>
+              <a href="https://www.linkedin.com/in/madhumita-sugandhi-557aa22a9/"
+                target='_blank'
+                rel='noopener noreferre'
+                className='text-gray-300 hover:text-white'
+              >
+                <FaLinkedin size={24} />
+              </a>
+            </div>
           </ul>
         </div>
       )}
